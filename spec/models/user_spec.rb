bc8e1fd154ba6ea4ee023e4ef4d72a0e9bfa71cd
David Wu <davidcnwu@gmail.com>
2015-05-12 20:44:45 -0700

    Some User model tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    describe "username" do
+      it "requires a username" do
+        user = User.new(email: "hobo@wufer.com", password: "test1234")
+        expect(user.save).to be(false)
+      end
+
+      it "requires that the username is unique" do
+        user = User.new(username: "hobowufer", email: "hobo@wufer.com", password: "test1234")
+        expect(user.save).to be(true)
+
+        bad_user = User.new(username: "hobowufer", email: "hobo@woofer.com", password: "test1234")
+        expect(bad_user.save).to be(false)
+      end
+    end
+
+    describe "email" do
+      it "requires an email" do
+      end
+
+      it "can use existing emails" do
+      end
+    end
+  end
 end
